017e41011cd996f9e7c68d48b696fe3d6540dc13
swadhinjit-das <swadhinjit@credencesoft.in>
2026-06-04 13:33:54 +0530

    Update PackagesSection.tsx

--- src/components/home/PackagesSection.tsx
+++ src/components/home/PackagesSection.tsx
@@ -138,7 +138,7 @@ export default function PackagesSection() {
         </div>
 
         {/* Package Cards */}
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 max-w-5xl mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 max-w-5xl mx-auto items-start">
           {packages.map((pkg) => {
             const Icon = pkg.icon
             const isOpen = expanded === pkg.id
